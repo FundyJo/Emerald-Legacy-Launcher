ae29dbae9d1f7153919e9d652a5cc5c758d47266
neoapps-dev <asd22.info@gmail.com>
2026-03-15 23:02:09 +0300

    feat: LegacyEvolved

--- src/services/versions.ts
+++ src/services/versions.ts
@@ -10,14 +10,20 @@ export const GAME_VERSIONS: GameVersion[] = [
   {
     id: "vanilla_tu19",
     name: "Vanilla Nightly (TU19)",
-    desc: "Leaked 4J Studios build.",
-    url: "https://huggingface.co/datasets/KayJann/emerald-legacy-assets/resolve/main/emerald_tu19_vanilla.zip"
+    desc: "Leaked 4J Studios build. (smartcmd)",
+    url: "https://github.com/smartcmd/MinecraftConsoles/releases/download/nightly/LCEWindows64.zip"
   },
   {
     id: "vanilla_tu24",
     name: "Vanilla TU24",
     desc: "Horses and Wither update.",
     url: "https://huggingface.co/datasets/KayJann/emerald-legacy-assets/resolve/main/emerald_tu24_vanilla.zip",
+  },
+  {
+    id: "legacy_evolved",
+    name: "Legacy Evolved",
+    desc: "Backporting the newer title updates back to the Minecraft: Legacy Console Edition Source Code Leak",
+    url: "https://github.com/piebotc/LegacyEvolved/releases/download/nightly/LCEWindows64.zip"
   },
   {
     id: "vanilla_tu75",
